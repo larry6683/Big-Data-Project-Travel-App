af444378c7aa087f0f358b6ecaee8d4913b4559a
emersonliu <emli8374@colorado.edu>
2026-04-03 23:19:33 -0600

    added standardized params

--- backend/tests/endpoints/test_weather.py
+++ backend/tests/endpoints/test_weather.py
@@ -12,46 +12,36 @@ class Test_Weather():
     check_in_date = str(today + relativedelta(days=1))
     check_out_date = str(today + relativedelta(days=2))
 
+    default_params = {
+        "lat": 10,
+        "lon": 10,
+        "check_in_date": check_in_date,
+        "check_out_date": check_out_date
+    }
+
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_pass(self, client):
-        response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 10,
-                "check_in_date": self.check_in_date,
-                "check_out_date": self.check_out_date
-            }
-        )
+    async def test_pass(self, client):
+        response = client.get(self.api_path, params= self.default_params)
 
         assert response.status_code == 200
         assert bool(response.json()) # Check that json is not empty
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_boundary_coordinates(self, client):
-        response = client.get(
-            self.api_path,
-            params = {
-                "lat": 90,
-                "lon": 180,
-                "check_in_date": self.check_in_date,
-                "check_out_date": self.check_out_date
-            }
-        )
+    async def test_boundary_coordinates(self, client):
+        response = client.get(self.api_path, params= self.default_params)
 
         assert response.status_code == 200
         assert bool(response.json()) # Check that json is not empty
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_same_day(self, client):
+    async def test_same_day(self, client):
         tomorrow = str(self.today + relativedelta(days= 1))
 
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 90,
-                "lon": 180,
-                "check_in_date": tomorrow,
+            self.api_path, 
+            params= {
+                **self.default_params, 
+                "check_in_date": tomorrow, 
                 "check_out_date": tomorrow
             }
         )
@@ -61,17 +51,16 @@ class Test_Weather():
 
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_forecast_after_30_days(self, client):
+    async def test_forecast_after_30_days(self, client):
         check_in_date_two_months_ahead = date.today() + relativedelta(months= 2)
         check_out_date_two_months_ahead = check_in_date_two_months_ahead + relativedelta(days= 1)
 
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 10,
-                "check_in_date": str(check_in_date_two_months_ahead),
-                "check_out_date": str(check_out_date_two_months_ahead)
+            self.api_path, 
+            params= {
+                **self.default_params, 
+                "check_in_date": check_in_date_two_months_ahead, 
+                "check_out_date": check_out_date_two_months_ahead
             }
         )
 
@@ -85,46 +74,35 @@ class Test_Weather():
     
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_bad_latitude(self, client):
+    async def test_bad_latitude(self, client):
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 999,
-                "lon": 10,
-                "check_in_date": self.check_in_date,
-                "check_out_date": self.check_out_date
-            }
+            self.api_path, 
+            params= {**self.default_params, "lat": 999}
         )
 
         assert response.status_code == 422
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_bad_longitude(self, client):
+    async def test_bad_longitude(self, client):
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 999,
-                "check_in_date": self.check_in_date,
-                "check_out_date": self.check_out_date
-            }
+            self.api_path, 
+            params= {**self.default_params, "lon": 999}
         )
 
         assert response.status_code == 422
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_invalid_date(self, client):
+    async def test_invalid_date(self, client):
         """
         Test invalid date using invalid leap year.
         """
         invalid_leap_year_2026 = "2026-02-29"
 
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 10,
-                "check_in_date": invalid_leap_year_2026,
+            self.api_path, 
+            params= {
+                **self.default_params, 
+                "check_in_date": invalid_leap_year_2026, 
                 "check_out_date": invalid_leap_year_2026
             }
         )
@@ -138,14 +116,15 @@ class Test_Weather():
         assert response.status_code == 422
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_past_dates(self, client):
+    async def test_past_dates(self, client):
+        check_in_date_2000 = "2000-01-01"
+        check_out_date_2000 = "2000-01-02"
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 10,
-                "check_in_date": "2000-01-01",
-                "check_out_date": "2000-01-02"
+            self.api_path, 
+            params= {
+                **self.default_params, 
+                "check_in_date": check_in_date_2000, 
+                "check_out_date": check_out_date_2000
             }
         )
 
@@ -153,24 +132,21 @@ class Test_Weather():
         assert response.json()['detail'] == "Cannot request weather for dates in the past"
 
     # @pytest.mark.skip(reason= "reduce api testing calls during development")
-    async def test_get_trip_weather_check_out_date_before_check_in(self, client):
+    async def test_check_out_date_before_check_in(self, client):
         check_in_date_swap = self.check_out_date
         check_out_date_swap = self.check_in_date
 
         response = client.get(
-            self.api_path,
-            params = {
-                "lat": 10,
-                "lon": 10,
-                "check_in_date": check_in_date_swap,
+            self.api_path, 
+            params= {
+                **self.default_params, 
+                "check_in_date": check_in_date_swap, 
                 "check_out_date": check_out_date_swap
             }
         )
 
         assert response.status_code == 400
         assert response.json()['detail'] == "Invalid date format (end, start)"
-    
-    # async def test_get_trip_weather_
     
     # @pytest.mark.skip(reason= "mocked test")
     # @patch('app.api.v1.endpoints.weather.weather_service.get_weather_for_trip', new_callable=AsyncMock)
