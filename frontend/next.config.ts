98618742aa376fed31188378d0421b625153a7a1
Sujith Battu <battusujith2525@gmail.com>
2026-04-12 14:41:14 -0600

    Backend folder alone moved to a totally new branch backend for easy local development

--- frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Add this line to ensure Turbopack correctly resolves and transpiles the library
-  transpilePackages: ['react-datepicker'],
-};
-
-export default nextConfig;
